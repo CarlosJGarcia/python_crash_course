c8ff50fc9ed0ce1bfe23465644501e0cfce9ae96
CarlosJGarcia <cgarciams@hotmail.com>
2025-10-17 10:11:11 +0200

    Bastantes modificaciones

--- space_invaders/settings.py
+++ space_invaders/settings.py
@@ -5,12 +5,13 @@ class Settings:
         """Initialize game settings"""
 
         # Screen settings
-        self.screen_width = 224 * 3
-        self.screen_height = 256 * 3
         self.frame_rate = 60
         self.bg_color = (0, 0, 0)
+        self.screen_width = 224 * 3
+        self.screen_height = 256 * 3
 
          # Ship settings
+        self.ship_limit = 2     # 3 Naves 0, 1 y 2 :-)
         self.ship_speed = 4.5
 
          # Bullet settings
@@ -19,3 +20,8 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (200, 200, 200)
         self.bullets_allowed = 1
+
+         # Alien/Fleet settings
+        self.alien_speed = 1.0
+        self.fleet_direction = 1
+        self.fleet_drop_speed = 10
